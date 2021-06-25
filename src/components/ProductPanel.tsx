390aa70cd302d9a7bd549314c46504dd1e83793e
Gareth Slinn <gslinn@gmail.com>
2021-06-25 21:17:00 +0100

    god begin link layout

--- src/components/ProductPanel.tsx
+++ src/components/ProductPanel.tsx
@@ -14,6 +14,10 @@ const ProductPanel = ({id, bodyType, modelName, modelType, imageUrl}: ItemProps)
             <h3 className="productPanel__bodyType">{bodyType}</h3>
             <h4 className="productPanel__modelName">{modelName}<span>{modelType}</span></h4>
             <img alt={`image of ` + modelName}  className="productPanel__image" src={imageUrl} />
+            <div className="productPanel__linkContainer">
+                <a href={`/learn/${id}`}>Learn <i className="icon icon-link-chevron">&nbsp;</i></a>
+                <a href={`/shop/${id}`}>Shop <i className="icon icon-link-chevron">&nbsp;</i></a>
+            </div>
         </div>
     );
 
